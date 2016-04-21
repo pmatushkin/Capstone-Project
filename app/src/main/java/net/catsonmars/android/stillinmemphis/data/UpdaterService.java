36b7aa651804f99cfd52d809cc602e56be28814b
pmatushkin <pmatushkin@gmail.com>
2016-04-21 19:55:17 -0400

    feat: Add OkHttp to the project
    Build a dummy updater service and use OkHttp to obtain a sample response.

--- app/src/main/java/net/catsonmars/android/stillinmemphis/data/UpdaterService.java
+++ app/src/main/java/net/catsonmars/android/stillinmemphis/data/UpdaterService.java
@@ -2,6 +2,15 @@ package net.catsonmars.android.stillinmemphis.data;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 /**
@@ -20,13 +29,52 @@ public class UpdaterService extends IntentService {
     public static final String EXTRA_REFRESHING
             = "net.catsonmars.android.stillinmemphis.intent.extra.REFRESHING";
 
+    String url = "http://production.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML=<TrackFieldRequest USERID=\"\"><TrackID ID=\"\"></TrackID></TrackFieldRequest>";
+
     public UpdaterService() {
         super(TAG);
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
+
+        try {
+            Log.d(TAG, run());
+        } catch (IOException io) {
+            Log.e(TAG, io.toString());
+        }
+
         sendBroadcast(new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, false));
         sendBroadcast(new Intent(BROADCAST_ACTION_DATA_CHANGE));
+    }
+
+    String run(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public String run() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Uri.Builder builder = new Uri.Builder();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+        Headers responseHeaders = response.headers();
+        for (int i = 0; i < responseHeaders.size(); i++) {
+            Log.d(TAG, responseHeaders.name(i) + ": " + responseHeaders.value(i));
+        }
+
+        return response.body().string();
     }
 }
