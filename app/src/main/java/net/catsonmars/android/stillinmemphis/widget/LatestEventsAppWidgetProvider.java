38c532db7ef9f7368fedef2b2ca236c71da33c92
pmatushkin <pmatushkin@gmail.com>
2016-05-12 20:30:53 -0400

    feat: Create stub classes for the 4x3 Latest Events resizable widget

---
+++ app/src/main/java/net/catsonmars/android/stillinmemphis/widget/LatestEventsAppWidgetProvider.java
@@ -0,0 +1,9 @@
+package net.catsonmars.android.stillinmemphis.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by pmatushkin on 5/12/2016.
+ */
+public class LatestEventsAppWidgetProvider extends AppWidgetProvider {
+}
